a5bd37169edda5c6d5f5a47622591a1bebd2c75f
Willi3 <gdarias1987@gmail.com>
2021-03-20 22:17:39 -0300

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gitlab.com/gdarias1987/serverApp
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gdarias1987/serverApp/controller"
+	"github.com/gdarias1987/serverApp/service"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	VideoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
+
+	server.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "OK!!",
+		})
+	})
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+	fmt.Println("Server running...")
+}
